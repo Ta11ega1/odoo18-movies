cc5e9cb76f31f70fa782975a1171f97db83630ba
Ignacio Segura <ignacio.segura@systcom.cl>
2025-04-07 02:12:28 -0400

    [ADD] Se agrega api para consulta de peliculas con mejor ranking

--- addons/movies/controllers/top_movies.py
+++ addons/movies/controllers/top_movies.py
@@ -7,7 +7,14 @@ from odoo.http import Controller, request, route
 
 class TopMovies(Controller):
 
-    @route('/api/top_movies', type='json', auth='public', methods=['GET'], website=True)
+    @route('/api/top_movies', type='json', auth='public', website=True)
     def get_combination_info_website(self):
         movies = request.env['movie.movie'].sudo().search([], order='ranking_movie desc', limit=10)
-        return movies
+        movie_list = []
+        for movie in movies:
+            movie_list.append({
+                'id': movie.id,
+                'movie_title': movie.movie_title,
+                'ranking_movie': movie.ranking_movie,
+            })
+        return json.dumps(movie_list)
